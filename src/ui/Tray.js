1bf0defb434cb40253c667e51d6a8a0ce9a73395
Guilherme-SG <silvagomesguilherme@hotmail.com>
2026-09-05 21:29:28 -0300

    fix: update icon path in Tray.js and remove old icon file

--- src/ui/Tray.js
+++ src/ui/Tray.js
@@ -10,7 +10,7 @@ class AppTray {
 
   create() {
     this.tray = new Tray(
-      path.join(__dirname, '..', 'assets', 'images', 'icon.png')
+      path.join(__dirname, '..', '..', 'assets', 'images', 'icon.png')
     );
 
     this.tray.setToolTip('Desktop Reminder');
